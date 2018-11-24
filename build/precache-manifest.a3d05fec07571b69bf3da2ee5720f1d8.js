e3cc850f701a32022118317093e8e02a1eec1bc0
jmasonherr <jmjmh@Jeremys-MacBook-Air.local>
2018-11-24 14:07:19 -0500

    spelligng

---
+++ build/precache-manifest.a3d05fec07571b69bf3da2ee5720f1d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "65453a6680d640d03550",
+    "url": "/texts-app/static/css/main.d682c724.chunk.css"
+  },
+  {
+    "revision": "65453a6680d640d03550",
+    "url": "/texts-app/static/js/main.65453a66.chunk.js"
+  },
+  {
+    "revision": "73a7a32f6bb303fe724f",
+    "url": "/texts-app/static/css/1.dc9f1478.chunk.css"
+  },
+  {
+    "revision": "73a7a32f6bb303fe724f",
+    "url": "/texts-app/static/js/1.73a7a32f.chunk.js"
+  },
+  {
+    "revision": "4b8fb61a75c2d2734267",
+    "url": "/texts-app/static/js/runtime~main.4b8fb61a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/texts-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b40ae230c359c076c632b80d41b2065c",
+    "url": "/texts-app/index.html"
+  }
+];
